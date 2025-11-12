9f0a2aa5600ed4625b4025ee7a5559dcff877f4a
tkt-test1 <mayuko.takita+git1@findy.co.jp>
2025-11-12 14:25:36 +0900

    Create user_controller.go

---
+++ go-task-api/controllers/user_controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"go-task-api/models"
+	"go-task-api/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Register(c *gin.Context) {
+	var input models.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(input.Password), 10)
+	user := models.User{Name: input.Name, Email: input.Email, Password: string(hashed)}
+	models.DB.Create(&user)
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
+
+func Login(c *gin.Context) {
+	var input models.User
+	var user models.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	if err := models.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, _ := utils.GenerateToken(user.Email)
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
